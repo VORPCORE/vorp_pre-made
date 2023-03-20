5612f769a078c8082cd777f27ed0d7e597e1d9dd
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-03-20 00:54:36 +0000

    hosing fix , missing files

--- server/resources/[vorp_official_plugins]/vorp_housing/fxmanifest.lua
+++ server/resources/[vorp_official_plugins]/vorp_housing/fxmanifest.lua
@@ -1,16 +1,12 @@
 game 'rdr3'
-fx_version 'adamant'
+
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_scripts {
-  'vorphousing_cl.net.dll'
-}
+client_scripts { '*.Client.net.dll' }
+server_scripts { '*.Server.net.dll' }
 
-server_scripts {
-  'vorphousing_sv.net.dll',
-}
+files { 'Newtonsoft.Json.dll', 'config.json', 'languages/**/*' }
 
-
-files {
-  'Newtonsoft.Json.dll',
-}
+-- Log Levels; none, trace, debug, info, warn, error, all
+log_level 'none'
