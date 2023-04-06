7aab9ca7ef68a90a7a1b9182f1a1f8b306873524
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2023-04-06 17:13:10 -0600

    updating fx version

--- server/resources/[Community]/weathersync/fxmanifest.lua
+++ server/resources/[Community]/weathersync/fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version "cerulean"
+fx_version "adamant"
 games {"gta5", "rdr3"}
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
