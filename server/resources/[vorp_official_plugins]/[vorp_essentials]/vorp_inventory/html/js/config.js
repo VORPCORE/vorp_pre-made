45a7345cff3e731534859b662b7c32ee1ff8d93c
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-04-29 14:49:32 +0100

    rename folder

--- server/resources/[vorp_official_plugins]/[vorp_essentials]/vorp_inventory/html/js/config.js
+++
@@ -1,7 +0,0 @@
-var Config = new Object();
-Config.closeKeys = [113, 27];
-Config.UseGoldItem = false;
-Config.AddGoldItem = false;
-Config.AddDollarItem = false;
-Config.AddAmmoItem = false;
-Config.DoubleClickToUse = true;
