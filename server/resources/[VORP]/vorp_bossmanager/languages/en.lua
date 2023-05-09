6db66aacde7e454e922d203098775d4e9ba3770f
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-05-09 15:08:09 +0100

    update vorp bossmanager

---
+++ server/resources/[VORP]/vorp_bossmanager/languages/en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+    ['title'] = 'Boss',
+    ['subtitle'] = 'Boss Menu',
+    ['subtitle2'] = 'Employee name',
+    ['subtitle3'] = 'Employee list',
+    ['sub_menu1'] = 'Hire',
+    ['sub_menu2'] = 'Resign',
+    ['gradelbl'] = 'Rank',
+    ['inputgrademsg'] = 'Ypu need the right Rank',
+    ['gradeerrormsg'] = 'Rank is not valid',
+    ['error_noclosestplayer'] = 'Player is not close enough',
+}
