e3427e6178b1494d212e0469555075fe80c6b3b6
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-10 09:11:02 +0000

    updates

--- server/resources/[VORP]/[vorp_essentials]/vorp_character/fxmanifest.lua
+++ server/resources/[VORP]/[vorp_essentials]/vorp_character/fxmanifest.lua
@@ -36,7 +36,7 @@ files {
 
 --========= VERSION =============--
 
-version '1.5'
+version '1.6'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_character-lua'
