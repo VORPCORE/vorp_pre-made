17c3457ce8c89a15d525030077c15a552d5b4e70
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-27 12:43:12 +0000

    Update fxmanifest.lua

--- server/resources/[VORP]/vorp_housing/fxmanifest.lua
+++ server/resources/[VORP]/vorp_housing/fxmanifest.lua
@@ -1,8 +1,9 @@
----@diagnostic disable: undefined-global
+
 game 'rdr3'
 
 fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+description 'Housing script for vorp core framework'
 
 client_scripts { '*.Client.net.dll' }
 server_scripts { '*.Server.net.dll' }
