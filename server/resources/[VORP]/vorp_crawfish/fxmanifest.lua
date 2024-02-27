034dae399943f0ba86b386556f6a0155f32784fc
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-27 12:38:39 +0000

    Update fxmanifest.lua

--- server/resources/[VORP]/vorp_crawfish/fxmanifest.lua
+++ server/resources/[VORP]/vorp_crawfish/fxmanifest.lua
@@ -1,19 +1,23 @@
-fx_version "adamant"
-games {"rdr3"}
+fx_version 'cerulean'
+game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
 author 'VORP @adamdrakon'
+description 'A script to catch crawfish for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_Crawfish'
+
 client_scripts {
-	'shared/locale.lua',
+    'shared/locale.lua',
     'languages/*.lua',
-	'config.lua',
-	'client/*.lua'
+    'config.lua',
+    'client/*.lua'
 }
 
 server_scripts {
-	'shared/locale.lua',
+    'shared/locale.lua',
     'languages/*.lua',
-	'config.lua',
-	'server/*.lua'
+    'config.lua',
+    'server/*.lua'
 }
 
 version '1.0'
